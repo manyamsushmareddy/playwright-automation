dd431ee65f1940c68c63b861970ba62cec40c2a8
Sushma <your-email@gmail.com>
2026-02-28 11:38:41 +0530

    saving automation script

--- tests/Dropdown.spec.js
+++ tests/Dropdown.spec.js
@@ -1,25 +1,31 @@
 const { test, expect } = require('@playwright/test');
 
 test('Handle select dropdown', async ({ page }) => {
-  await page.goto('https://demoqa.com/select-menu');
-  await page.pause();  
+  await page.goto('https://testautomationpractice.blogspot.com');
 
-  // Select by visible text
-await page.click('#multiselect-dropdown');   // open dropdown
-await page.click('text=Red');  
-         // select option
+  await page.locator('[placeholder="Enter Name"]').fill('Sushma');
+  await page.fill('#email','sushma@gmail.com');
+  await page.locator('[placeholder="Enter Phone"]').fill('1234567890');
+  await page.locator('[id="textarea"]').fill('Hitech city hyderabad');
+  await page.locator('[id="female"]').click('female');
+  await page.locator('[id="sunday"]').click('sunday','monday');
+  await page.locator('[id="country"]').selectOption('India');
+  await page.locator('[id="colors"]').selectOption('yellow');
+  await page.locator('[id="animals"]').selectOption('Cat');
+
+  page.locator('[placeholder="Date Picker 1 (mm/dd/yyyy)"]','01/01/2025');
+  await page.locator('[placeholder="Date Picker 2 (mm/dd/yyyy)"]','01/01/2026');
+  
+
+  await page.locator('[placeholder="Start Date"]','01/01/2025');
+  await page.locator('[placeholder="End Date"]','01/01/2027');
+  await page.locator('[id="submit"]').click();
+
+  await page.waitForTimeout(5000);
+
+
+
 
   
 }); 
-/*
-const { test, expect } = require('@playwright/test');
 
-test('Handle radio button', async ({ page }) => {
-
-  await page.goto('https://demoqa.com/radio-button');
-
-  await page.locator('#yesRadio').check();
-
-  await expect(page.locator('.text-success')).toHaveText('Yes');
-});
-*/
